fe7ce3bac2d26fe0fb379072f2e1649eef552157
rezende-lucas <34248146+rezende-lucas@users.noreply.github.com>
2025-03-11 02:47:05 +0000

    Deploying to gh-pages from @ rezende-lucas/photo-album@be7da2bcb45c62ca828b63732031f1c0d9602174 🚀

---
+++ js/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://djtopteopfkzbnjdnfgo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdG9wdGVvcGZremJuamRuZmdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NTI2MDEsImV4cCI6MjA1NjUyODYwMX0.w4BL7ZSxJqSvOTlmN3QZJ4c6CFKkDr_BYgn4qye5dc8';
