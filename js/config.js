b9e6726642939fb396ced7992fad8e1df1a036a9
Lucas Rezende Ferreira Carneiro <34248146+rezende-lucas@users.noreply.github.com>
2025-03-10 23:07:14 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// EXEMPLO - Substitua com suas credenciais reais e renomeie para config.js
+const SUPABASE_URL = 'https://djtopteopfkzbnjdnfgo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdG9wdGVvcGZremJuamRuZmdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA5NTI2MDEsImV4cCI6MjA1NjUyODYwMX0.w4BL7ZSxJqSvOTlmN3QZJ4c6CFKkDr_BYgn4qye5dc8';
